294d43fb8f5d3ba0545e0c876f58aa245b835273
Juan Bermudez Roldan <>
2018-08-27 21:13:57 +0200

    Add Login class

--- src/main/java/com/juanbrz/componentexample/service/LoginService.java
+++ src/main/java/com/juanbrz/componentexample/service/LoginService.java
@@ -4,6 +4,12 @@ import com.juanbrz.componentexample.model.User;
 
 public interface LoginService {
 
+  /**
+   * Return the logged user
+   * @param username Username
+   * @param password Password
+   * @return User object
+   */
   User login(String username, String password);
 
 }
--- src/main/java/com/juanbrz/componentexample/service/LoginServiceImpl.java
+++ src/main/java/com/juanbrz/componentexample/service/LoginServiceImpl.java
@@ -10,16 +10,32 @@ public class LoginServiceImpl implements LoginService{
 
   private static final String NO_PASSWORD = "###";
 
+  /**
+   * A component won't change its reference,
+   * and it only can be set by constructor
+   */
   private final DatabaseComponent databaseComponent;
 
+  /**
+   * Inject the component
+   * @param databaseComponent
+   */
   @Autowired
   public LoginServiceImpl(final DatabaseComponent databaseComponent){
     this.databaseComponent = databaseComponent;
   }
 
+  /**
+   * We dont want to return the password
+   * @param username
+   * @param password
+   * @return the logged user without password
+   */
   @Override
   public User login(String username, String password) {
     User user = databaseComponent.getUser(username, password);
+    // This should be done already in the databaseComponent.
+    // Password should not leave the database.
     user.setPassword(NO_PASSWORD);
     return user;
   }
